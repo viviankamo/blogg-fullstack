1301ac785cbbbe8045d7c8bb4d2d87c64506af82
viviankamo <vivian.kamo@elev.ga.ntig.se>
2021-01-22 11:23:57 +0100

    Book-blogg

---
+++ models/article.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const marked = require('marked')
+const slugify = require('slugify')
+const createDomPurify = require('dompurify')
+const { JSDOM } = require('jsdom')
+const dompurify = createDomPurify(new JSDOM().window)
+
+const articleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: String
+  },
+  summary: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  sanitizedHtml: {
+    type: String,
+    required: true
+  }
+})
+
+articleSchema.pre('validate', function(next) {
+  if (this.title) {
+    this.slug = slugify(this.title, { lower: true, strict: true })
+  }
+
+  if (this.summary) {
+    this.sanitizedHtml = dompurify.sanitize(marked(this.summary))
+  }
+
+  next()
+})
+
+module.exports = mongoose.model('Article', articleSchema)
